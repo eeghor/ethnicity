b5320ad8c1945e93f6691bb69c73b3d19664001a
Igor Korostil <eeghor@gmail.com>
2018-04-10 18:22:13 +1000

    added german

--- ethnicity/ethnicity.py
+++ ethnicity/ethnicity.py
@@ -3,9 +3,10 @@ import json
 from unidecode import unidecode
 import pandas as pd
 
+import math
 import re
 import time
-from collections import defaultdict
+from collections import defaultdict, Counter
 from string import ascii_lowercase
 
 class Ethnicity(object):
@@ -14,68 +15,24 @@ class Ethnicity(object):
 	get ethnicity from name
 	"""
 	
-	def __init__(self, race_thresh=70):
+	def __init__(self):
 
 		self.DATADIR = os.path.join(os.path.curdir, 'data')
 
 		self.ETHNICITIES = set("""indian japanese greek arabic turkish
 									thai vietnamese balkan italian samoan
-										hawaiian khmer chinese korean polish fijian english""".split())
+										hawaiian khmer chinese korean polish fijian english german""".split())
 		
 		assert self.ETHNICITIES <= set(os.listdir(self.DATADIR)), '** error ** data is missing for some ethnicities!'
 
 		print(f'ethnicities: {len(self.ETHNICITIES)}')
-		
-		self.ETHNIC_NAMES_U = json.load(open(os.path.join(self.DATADIR, 'data_names_u_.json'), 'r'))
-		print(f'unique names: {len({n for l in self.ETHNIC_NAMES_U for n in self.ETHNIC_NAMES_U[l]})}')
-
-		self.ETHNIC_ENDINGS_U = json.load(open(os.path.join(self.DATADIR, 'data_surname_end_u_.json'), 'r'))
-		print(f'unique surname endings: {len({n for l in self.ETHNIC_ENDINGS_U for n in self.ETHNIC_ENDINGS_U[l]})}')
-
-		"""
-		create a race dictionary (the US Census data); it should be like this:
-		{'s': defaultdict(None,
-						 {'smith': 'white',
-						  'sanchez': 'latino',
-						  'sullivan': 'white',
-						  'stevens': 'white',
-						  'simpson': 'white'
-		"""
-		self.RACE_DIC = defaultdict(lambda: defaultdict())
-
-		race = {'pctwhite': 'white', 
-				'pctblack': 'black', 
-				'pctapi': 'asian', 
-				'pctaian': 'native', 
-				'pct2prace': 'mixed', 
-				'pcthispanic': 'latino'}
-
-		d = pd.read_csv(os.path.join(self.DATADIR, 'Names_2010Census.csv')).iloc[:,[0,5,6,7,8,9,10]].dropna()
-		d['name'] = d['name'].str.lower()
-		d.iloc[:,1:] = d.iloc[:,1:].applymap(lambda x: 0.0 if not str(x).replace('.','').isdigit() else x)
-		d['max'] = d.iloc[:,1:].astype(float).max(axis=1)
-		d['race'] = (d.iloc[:,1:-1].astype(float).idxmax(axis=1)
-					.where(d['max'] > race_thresh, None)
-					 .apply(lambda x: race.get(x, None)))
-		d.drop('max', axis=1, inplace=True)
-		d = d[d['race'].notnull()]
-
-		for r in d.iterrows():
-			_name = r[1]['name']
-			self.RACE_DIC[_name[0]][_name] = r[1]['race']
-
-		print(f'created race dictionary with the likeliest race for {len({s for l in self.RACE_DIC for s in self.RACE_DIC[l]})} surnames')
 
 		# map the included ethnicities to race
 		self.RACE_TO_ETHN = {'asian': ['indian', 'japanese', 'vietnamese', 'chinese', 'korean', 'khmer', 'thai'],
-							 'white': ['greek', 'turkish', 'balkan', 'italian', 'polish'],
+							 'white': ['greek', 'turkish', 'balkan', 'italian', 'polish', 'english', 'german'],
 							 'black': ['arabic']}
-
-		self.SEPARATORS = re.compile(r'[-,_/().]')
 		
-		# make name and surname dictionaries by letter for required ethnicities
-		self.names = dict()
-		self.surnames = dict()
+		self.SEPARATORS = re.compile(r'[-,_/().]')
 
 	def __readtext(self, file):
 		"""
@@ -91,7 +48,12 @@ class Ethnicity(object):
 			for _ in sorted(list(what)):
 				f.write(f'{unidecode(_.strip())}\n')
 
-	def _make_name_dict(self, refresh=True):
+	def make_dicts(self, refresh=True):
+		"""
+		create a dictionary like this:
+		{'korean': {'full_names': ['name1', 'name2',..],
+					'first_names': ['name1', 'name2',..]}}
+		"""
 
 		d = defaultdict(lambda: defaultdict())
 
@@ -107,34 +69,79 @@ class Ethnicity(object):
 				except:
 					print(f'warning: can\'t find {e} {_}!')		
 
-		return d
 
-	def setup(self):
+		self.ETHNIC_NAMES_U = defaultdict(lambda: defaultdict(set))
+		self.ETHNIC_SURNAMES_U = defaultdict(lambda: defaultdict(set))
+		self.ETHNIC_ENDINGS_U = defaultdict(lambda: defaultdict(set))
 
-		d = self._make_name_dict()
-
-		du = defaultdict(lambda: defaultdict(list))
-		dx = defaultdict(lambda: defaultdict(set))
+		# create a dictionary like {'a': 'aaron': ['english', 'fijian'],
+		#  								   'abu': ['polish', 'khmer']}
 
 		for e in d:
+
 			for n in d[e]['first_names']:
-				du[n[0]][n].append(e)
+				self.ETHNIC_NAMES_U[n[0]][n].add(e)
 
-		for e in d:
+			# and another dictionary for last name endings like
+			# {'b': 'sky': ['japanese', 'italian'],
+			# 		'ozzi': ['italian']}
+
 			if 'last_names' in d[e]:
 				for n in d[e]['last_names']:
-					for s in range(3,6):
+					self.ETHNIC_SURNAMES_U[n[0]][n].add(e)
+
+			# extras: add the most popular names/surnames from the full names if available
+			if 'full_names' in d[e]:
+				for name in ['first_names', 'last_names']:
+					if name in d[e]:
+						pick_idx = -1 if (name == 'last_names') else 0
+						lst = [full_name.split()[pick_idx] for full_name in d[e]['full_names']]
+						for _ in Counter(lst).most_common(math.floor(0.8*len(lst))):
+							name_ = _[0]
+							if name == 'last_names':
+								if _[1] > 1:
+									self.ETHNIC_SURNAMES_U[name_[0]][name_].add(e)
+							elif name == 'first_names':
+								if _[1] > 3:
+									self.ETHNIC_NAMES_U[name_[0]][name_].add(e)
+
+			# dictionary for surname endings
+			for l in self.ETHNIC_SURNAMES_U:
+				for n in self.ETHNIC_SURNAMES_U[l]:
+					for s in range(3,6):  # note: 3,4,5
 						_ = n[-s:]
-						dx[_[0]][_].add(e)
+						self.ETHNIC_ENDINGS_U[_[0]][_].add(e)
+		
 
-		self.ETHNIC_NAMES_U = du
-		self.ETHNIC_ENDINGS_U = dx
+		# create a race dictionary like 
+		# {'d': {'david': 'white', 'diego': 'latino'}}
 
-		return self
+		self.RACE_DIC = defaultdict(lambda: defaultdict())
 
+		race = {'pctwhite': 'white', 
+				'pctblack': 'black', 
+				'pctapi': 'asian', 
+				'pctaian': 'native', 
+				'pct2prace': 'mixed', 
+				'pcthispanic': 'latino'}
 
+		d = pd.read_csv(os.path.join(self.DATADIR, 'Names_2010Census.csv')).iloc[:,[0,5,6,7,8,9,10]].dropna()
+		d['name'] = d['name'].str.lower()
+		d.iloc[:,1:] = d.iloc[:,1:].applymap(lambda x: 0 if not str(x).replace('.','').isdigit() else x)
+		d['max'] = d.iloc[:,1:].astype(float).max(axis=1)
+		d['race'] = (d.iloc[:,1:-1].astype(float).idxmax(axis=1)
+					.where(d['max'] > 70, None)
+					 .apply(lambda x: race.get(x, None)))
+		d.drop('max', axis=1, inplace=True)
+		d = d[d['race'].notnull()]
+
+		for r in d.iterrows():
+			_name = r[1]['name']
+			self.RACE_DIC[_name[0]][_name] = r[1]['race']
 
 		
+		return self
+	
 	def _normalize(self, st):
 		
 		if not isinstance(st, str):
@@ -159,40 +166,65 @@ class Ethnicity(object):
 	 
 		return _st
 
-	def _by_letter(self, name, what):
+	def search_names(self, word):
 		"""
-		find unique name or surname by letter
+		find word in the name dictionary (arranged by letter)
 		"""
-		assert what in 'name surname_ending race'.split(), f'function _by_letter must receive a valid what value!'
+		return self.ETHNIC_NAMES_U[word[0]].get(word, None)
 
-		if what == 'name':	
+	def search_surnames(self, word):
+		"""
+		find word in the surname dictionary (arranged by letter)
+		"""
+		return self.ETHNIC_SURNAMES_U[word[0]].get(word, None)
 
-			_l1 = name[0]
+	def search_surname_endings(self, word):
+		"""
+		find word in the surname ending dictionary (arranged by letter)
+		"""
+		return self.ETHNIC_ENDINGS_U[word[0]].get(word, None)
+
+	def search_race(self, word):
+		"""
+		find race
+		"""
+		return self.RACE_DIC[word[0]].get(word, None)
+
+
+	# def _by_letter(self, name, what):
+	# 	"""
+	# 	find unique name or surname by letter
+	# 	"""
+	# 	assert what in 'name surname_ending race'.split(), f'function _by_letter must receive a valid what value!'
+
+	# 	if what == 'name':	
+
+	# 		_l1 = name[0]
 	
-			try:
-				return self.ETHNIC_NAMES_U[_l1].get(name, None)
-			except:
-				return None
+	# 		try:
+	# 			return self.ETHNIC_NAMES_U[_l1].get(name, None)
+	# 		except:
+	# 			return None
 
-		elif what == 'surname_ending':
+	# 	elif what == 'surname_ending':
 
-			for last in range(4, 1, -1):
+	# 		for last in range(4, 1, -1):
 
-				_ = name[-last:]
+	# 			_ = name[-last:]
 
-				_l1 = _[0]
+	# 			_l1 = _[0]
 
-				try:
-					return self.ETHNIC_ENDINGS_U[_l1][_]
-				except:
-					continue
+	# 			try:
+	# 				return self.ETHNIC_ENDINGS_U[_l1][_]
+	# 			except:
+	# 				continue
 
-		elif what == 'race':
+	# 	elif what == 'race':
 
-			try:
-				return self.RACE_DIC[name[0]].get(name, None)
-			except:
-				return None
+	# 		try:
+	# 			return self.RACE_DIC[name[0]].get(name, None)
+	# 		except:
+	# 			return None
 
 
 	def _split_name_surname(self, st):
@@ -216,23 +248,21 @@ class Ethnicity(object):
 
 		return (_name, _surname)
 
-	def _find_unique(self, name, surname):
+	# def _find_unique(self, name, surname):
 
-		_name, _surname = name, surname
+	# 	_name, _surname = name, surname
 
-		print('_surname=',_surname)
+	# 	if _name:
+	# 		eth_unique_name = self._by_letter(_name, 'name')
+	# 		if eth_unique_name:
+	# 			return eth_unique_name
 
-		if _name:
-			eth_unique_name = self._by_letter(_name, 'name')
-			if eth_unique_name:
-				return eth_unique_name
-
-		if _surname:
-			eth_surn_ending = self._by_letter(_surname, 'surname_ending')
-			rc = self._by_letter(_surname, 'race')
-			print('rc=', rc)
-			if eth_surn_ending:
-				return eth_surn_ending
+	# 	if _surname:
+	# 		eth_surn_ending = self._by_letter(_surname, 'surname_ending')
+	# 		rc = self._by_letter(_surname, 'race')
+	# 		print('rc=', rc)
+	# 		if eth_surn_ending:
+	# 			return eth_surn_ending
 
 	
 	def get(self, st):
@@ -244,18 +274,19 @@ class Ethnicity(object):
 
 		_name, _surname = self._split_name_surname(st)
 
-		ethnicity = self._find_unique(_name, _surname)
+		print(f'_name = {_name} _surname = {_surname}')
 
-		if not ethnicity:
-			ethnicity = self._find_unique(_surname, _name)
+		print(f'found names: {self.search_names(_name)}')
+		print(f'found surnames: {self.search_surnames(_surname)}')
+		print(f'found surname endings: {self.search_surname_endings(_surname)}')
+		print(f'found race: {self.search_race(_surname)}')
 
-		self._find_unique(_name, _surname)
 
-		return ethnicity
+		# return ethnicity
 				
 
 if __name__ == '__main__':
 
-	e = Ethnicity(race_thresh=67.5).setup()
+	e = Ethnicity().make_dicts()
 
-	print(e.get('apaitia hodgson'))
+	print(e.get('matthias novak'))
